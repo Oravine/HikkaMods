563b715e0ad5ad135aa6499c94eeaa512ea84b75
Oravine <ivliev.vlad.s.3010@gmail.com>
2025-04-20 22:50:24 +0300

    Update MoneyEVO.py

--- MoneyEVO.py
+++ MoneyEVO.py
@@ -1,3 +1,8 @@
+# https://github.com/Oravine/HikkaMods
+
+# You can redistribute it and/or modify it under the terms of the GNU AGPLv3
+# https://www.gnu.org/licenses/agpl-3.0.html
+
 #Created by @Oravine
 #Hikka Module
 
